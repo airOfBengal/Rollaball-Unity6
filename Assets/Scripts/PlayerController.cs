80a7c6a9c69dec077da89e9a03661bc65aaa2355
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2025-08-15 11:10:03 +0600

    add player move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 5.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+}
